882f2adf0e089205678c3c87ec41337c8c3c9bcf
Muhamad Faiz Azhar <tuan2fayofficial@gmail.com>
2022-01-17 21:00:19 +0700

    Update anti-tor.sh

--- anti-tor.sh
+++ anti-tor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 iptables -N TOR
+iptables -A TOR -j LOG --log-prefix="[TOR BLOCKED] "
 iptables -A TOR -j DROP
 
 curl -sk "https://check.torproject.org/torbulkexitlist" | xargs -I {} -n1 bash -c 'iptables -A INPUT -s "{}" -p tcp -m multiport --dports 80,443 -j TOR;echo "IP TOR : {} => Blocked"' _
